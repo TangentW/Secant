d95688b122044dac1d6164a5b689e394f08958a4
Tangent <tangentsw@gmail.com>
2020-06-29 02:24:44 +0800

    Tweak Demo

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -30,14 +30,22 @@ struct DemoForm: Form {
     
     @State
     var isEnabled = true
+    
+    @State
+    var isHidden = false
 
     @RowsBuilder
     var rows: Rows {
-        Switch(title: "Expand", isOn: $isExpanded)
-        Switch(title: "Toggle", isOn: $isEnabled)
-            .onRender {
-                $0.cell.textLabel?.textColor = .red
+        Switch(title: "Hide", isOn: $isHidden)
+        
+        if !isHidden {
+            Switch(title: "Expand", isOn: $isExpanded)
+            Switch(title: "Toggle", isOn: $isEnabled)
+                .onRender {
+                    $0.cell.textLabel?.textColor = .red
             }
+        }
+
         Label("Hello World")
             .detail(isEnabled ? "⭕️" : "❌")
             .height(isExpanded ? 150 : 50)
